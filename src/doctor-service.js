6d59027469097bf4b73dfafa715748b67d51826d
megan schulte <megan.schulte.pdx@gmail.com>
2019-06-07 10:05:35 -0700

    add index.html with basic search form

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -8,9 +8,6 @@ export class DoctorSearch {
 
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5155,122.6793&query=${illness}&first_name=${firstName}&last_name=${lastName},100&skip=2&limit=10&user_key=${key}`;
 
-
-      let url = `https://api.unsplash.com/photos/random?client_id=${key}&query=${userTheme}&orientation=landscape`;
-
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
