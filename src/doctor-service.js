fa9b534c73cb10a29c9ef93f20261622d6850565
megan schulte <megan.schulte.pdx@gmail.com>
2019-06-07 09:49:32 -0700

    add API call url and search criteria

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -4,7 +4,10 @@ export class DoctorSearch {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
 
-      let key = process.env.API_KEY;
+      let key = process.env.exports.apiKey;
+
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5155,122.6793&query=${illness}&first_name=${firstName}&last_name=${lastName},100&skip=2&limit=10&user_key=${key}`;
+
 
       let url = `https://api.unsplash.com/photos/random?client_id=${key}&query=${userTheme}&orientation=landscape`;
 
