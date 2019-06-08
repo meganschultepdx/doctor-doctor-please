52d5b470be7e9776547af0793377ce851f9d4356
megan schulte <megan.schulte.pdx@gmail.com>
2019-06-07 17:05:03 -0700

    change response paths to try to access correct response data

--- src/doctorSearch.js
+++ src/doctorSearch.js
@@ -4,9 +4,7 @@ export class DoctorSearch {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
 
-      let key = process.env.exports.apiKey;
-
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5155,122.6793&query=${illness}&name=${name},100&skip=2&limit=10&user_key=${key}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&name=${name}&location=or-portland&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function () {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -25,15 +25,15 @@ $(document).ready(function() {
       if(body.meta.count === 0) {
         $('.noResults').text('Sorry, there are no results that match your search.')
       } else {
-        $('.showFirstName').text(`First Name:  ${data.profile.first_name}`);
-        $('.showLastName').text(`Last Name:  ${data.profile.last_name}`);
-        $('.showPractice').text(`Practice: ${data.practice.name}`);
-        $('.showAddress').text(`Address:  ${data.practice.visit_address}`);
-        $('.showPhones').text(`Phone Number(s):  ${data.practice.phones}`);
-        $('.showWebsite').text(`Website:  ${data.practice.website}`);
-        $('.showNewPatients').text(`Accepts New Patients:  ${data.practice.accepts_new_patients}`);
-        $('.showGender').text(`Gender:  ${data.profile.gender}`);
-        $('.showLanguages').text(`Languages:  ${data.profile.languages}`);
+        $('.showFirstName').text(`First Name:  ${body.data.profile.first_name}`);
+        $('.showLastName').text(`Last Name:  ${body.data.profile.last_name}`);
+        $('.showPractice').text(`Practice: ${body.data.practice.name}`);
+        $('.showAddress').text(`Address:  ${body.data.practice.visit_address}`);
+        $('.showPhones').text(`Phone Number:  ${body.data.practice.phones[0]}`);
+        $('.showWebsite').text(`Website:  ${body.data.practice.website}`);
+        $('.showNewPatients').text(`Accepts New Patients:  ${body.data.practice.accepts_new_patients}`);
+        $('.showGender').text(`Gender:  ${body.data.profile.gender}`);
+        $('.showLanguages').text(`Languages:  ${body.data.profile.languages}`);
       }
 
     }, function(error) {
